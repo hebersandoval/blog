2c0fd8c2cfb4b124a0b202b05d5575049ec7c43f
Heber Sandoval <he3ers@gmail.com>
2016-05-05 17:11:16 -0400

    Fix typo in db adapter.

--- config/environment.rb
+++ config/environment.rb
@@ -4,6 +4,8 @@ require 'bundler/setup'
 Bundler.require(:default, ENV['SINATRA_ENV'])
 
 ActiveRecord::Base.establish_connection(
-  :adapter => "sqlite"
+  :adapter => "sqlite3",
   :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
 )
+
+require_all 'app'
