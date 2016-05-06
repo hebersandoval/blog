df69926e5229e6ee678d8933953d7b12469e3d40
Heber Sandoval <he3ers@gmail.com>
2016-05-06 07:54:29 -0400

    Create template for needed CRUD actions and to render the respective view.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,4 +4,29 @@ class PostsController < ApplicationController
     erb :'posts/index'
   end
 
+  get '/posts/new' do # create action
+    erb :'posts/new'
+  end
+
+  post '/posts' do
+    # get input from user via params
+  end
+
+  get '/posts/:id' do # show action
+    erb :'posts/show'
+  end
+
+  get '/posts/:id/edit' do # update action
+    erb :'/posts/edit'
+  end
+
+  patch '/posts/:id' do
+    # get input from user via params to update db
+  end
+
+  delete '/posts/:id/delete' do
+    # @post.delete
+    # redirect to '/posts'
+  end
+
 end
