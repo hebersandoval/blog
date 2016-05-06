4b6045e9bfaca36e9d83141a0ab6772fd113f6ed
Heber Sandoval <he3ers@gmail.com>
2016-05-06 08:24:35 -0400

    Create template for CRUD actions needed in the users_controller to corresponds to their views and mounted the controller in config.ru file.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,25 @@
 class UsersController < ApplicationController
 
+  get '/signup' do
+    erb :'users/signup'
+  end
+
+  post '/signup' do
+    # get input from user via params also provide validations
+  end
+
+  get '/login' do
+    erb :'/users/login'
+  end
+
+  post '/login' do
+    # get input for validation
+    #redirect to '/posts'
+  end
+
+  get '/logout' do
+    # session.clear
+    # redirect to '/'
+  end
+
 end
