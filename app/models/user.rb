630bea60ab118a91bd83377a069686fb300d869b
Heber Sandoval <he3ers@gmail.com>
2016-05-13 15:44:21 -0400

    Add helpful comments.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   has_many :posts
-  has_secure_password
-  validates_presence_of :username, :email, :password
+  has_secure_password # this is a macro that gives us some methods like .authenticate to use in our controller action
+  validates_presence_of :username, :email, :password # validating attributes to make sure no one can sign up without inputting  their name, email and password
 
   def slug
     self.username.downcase.gsub(" ", "-")
